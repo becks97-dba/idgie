10e044dc019eb413333b57c388a95a4f6a4a0124
becks97-dba <becks97@gmail.com>
2026-04-23 23:21:37 -0500

    oura.js

--- netlify/functions/oura.js
+++ netlify/functions/oura.js
@@ -1,13 +1,28 @@
+const https = require("https");
+
+function httpsGet(url, headers) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: { ...headers, "User-Agent": "IDGIE-App" }
+    };
+    https.get(url, options, (res) => {
+      let data = "";
+      res.on("data", chunk => { data += chunk; });
+      res.on("end", () => {
+        try { resolve(JSON.parse(data)); }
+        catch (e) { reject(e); }
+      });
+    }).on("error", reject);
+  });
+}
+
 exports.handler = async (event) => {
   const token = process.env.REACT_APP_OURA_TOKEN;
   const { endpoint, start_date, end_date } = event.queryStringParameters;
   const url = `https://api.ouraring.com/v2/usercollection/${endpoint}?start_date=${start_date}&end_date=${end_date}`;
-  
+
   try {
-    const res = await fetch(url, {
-      headers: { Authorization: `Bearer ${token}` }
-    });
-    const data = await res.json();
+    const data = await httpsGet(url, { Authorization: `Bearer ${token}` });
     return {
       statusCode: 200,
       headers: { "Access-Control-Allow-Origin": "*" },
